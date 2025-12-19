41303e5a9484ab0495cffa976572e504d233556e
jhin <joojh501@gmail.com>
2025-12-20 00:06:44 +0900

    feat(place): 서울시 실시간 도시데이터 API SERVICE 구현.

---
+++ src/main/java/com/github/jhinslog/onpopserver/place/service/SeoulApiService.java
@@ -0,0 +1,67 @@
+package com.github.jhinslog.onpopserver.place.service;
+
+import com.github.jhinslog.onpopserver.place.dto.seoul.SeoulApiDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SeoulApiService {
+
+    private final WebClient.Builder webClientBuilder;
+
+    @Value("${seoul.api.key}")
+    private String apiKey;
+
+    @Value("${seoul.api.url}")
+    private String baseUrl;
+
+    @Value("${seoul.api.type}")
+    private String type;
+
+    @Value("${seoul.api.service}")
+    private String serviceName;
+
+    @Value("${seoul.api.start_index}")
+    private int startIndex;
+
+    @Value("${seoul.api.end_index}")
+    private int endIndex;
+
+    public SeoulApiDto fetchCityData(String areaName) {
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new IllegalStateException("서울시 API 키가 설정되지 않았습니다.");
+        }
+
+        URI uri = UriComponentsBuilder.fromUriString(String.valueOf(baseUrl))
+                .pathSegment(apiKey, type, serviceName, String.valueOf(startIndex), String.valueOf(endIndex), areaName)
+                .build()
+                .encode(StandardCharsets.UTF_8)
+                .toUri();
+
+        log.info("Fetching data from Seoul API: {}", areaName);
+        log.debug("Request URI: {}", uri);
+
+        try {
+            return webClientBuilder.build()
+                    .get()
+                    .uri(uri)
+                    .retrieve()
+                    .bodyToMono(SeoulApiDto.class)
+                    .block();
+        } catch (Exception e) {
+            log.error("Failed to fetch data for area: {}. error: {}", areaName, e.getMessage());
+            return null;
+        }
+
+    }
+
+}
