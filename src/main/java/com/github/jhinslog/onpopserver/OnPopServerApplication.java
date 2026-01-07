704e0b4440e3886729bd07d0a06529a001d8ca20
jhin <joojh501@gmail.com>
2026-01-08 08:51:18 +0900

    fix: OnPopServerApplication @EnableScheduling 추가

--- src/main/java/com/github/jhinslog/onpopserver/OnPopServerApplication.java
+++ src/main/java/com/github/jhinslog/onpopserver/OnPopServerApplication.java
@@ -3,7 +3,9 @@ package com.github.jhinslog.onpopserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class OnPopServerApplication {
